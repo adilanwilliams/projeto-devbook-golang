d237bdf0c73bbda6a5aad2786a7bea6603c3b2e3
Adilan Williams <148641231+adilanwilliams@users.noreply.github.com>
2026-02-26 16:52:20 -0300

    Validação de usuário

--- api/src/models/user.go
+++ api/src/models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"strings"
+)
+
 // User represent a user in database.
 type User struct {
 	ID       uint64 `json:"id,omitempty"`
@@ -7,4 +12,40 @@ type User struct {
 	Email    string `json:"email,omitempty"`
 	Username string `json:"username,omitempty"`
 	Password string `json:"password,omitempty"`
+}
+
+
+// Prepare valida and formating the user.
+func (u *User) Prepare() error {
+	if err := u.validate(); err != nil{
+		return err
+	}
+
+	u.format()
+	return nil
+}
+
+func (u User) validate() error {
+	if u.Name == "" {
+		return errors.New("Name is required.")
+	}
+
+	if u.Email == "" {
+		return errors.New("Email is required.")
+	}
+
+	if u.Username == "" {
+		return errors.New("Username is required.")
+	}
+
+	if u.Password == "" {
+		return errors.New("Password is required.")
+	}
+
+	return nil
+}
+
+func (u *User) format() {
+	u.Name = strings.TrimSpace(u.Name)
+	u.Email = strings.TrimSpace(u.Email)
 }
